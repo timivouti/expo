c91f4170d8c0c9e5c16cdde43f799b9398cefe7c
Stanisław Chmiela <sjchmiela@gmail.com>
2020-02-25 14:06:02 +0100

    [expo-notifications] Implement notification scheduling API

--- packages/expo-notifications/build/index.js
+++ packages/expo-notifications/build/index.js
@@ -1,6 +1,9 @@
 export { default as getDevicePushTokenAsync } from './getDevicePushTokenAsync';
 export { default as getExpoPushTokenAsync } from './getExpoPushTokenAsync';
 export { default as presentNotificationAsync } from './presentNotificationAsync';
+export { default as getAllScheduledNotificationsAsync } from './getAllScheduledNotificationsAsync';
+export { default as scheduleNotificationAsync } from './scheduleNotificationAsync';
+export { default as cancelScheduledNotificationAsync } from './cancelScheduledNotificationAsync';
 export * from './TokenEmitter';
 export * from './NotificationsEmitter';
 export * from './NotificationsHandler';
